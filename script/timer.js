e392a7b2c54db59a9f3d2196dcd1cc21ade63544
Golishon0 <jaonja69@yandex.ru>
2021-05-17 17:45:44 +0300

    timer

---
+++ script/timer.js
@@ -0,0 +1,18 @@
+var timer_life = 20245;
+
+var timer = setInterval(function () {
+  timer_life -= 1;
+
+  if (timer_life == 0) {
+    clearInterval(timer);
+  }
+
+  let w = Math.trunc(timer_life / 3600),
+    m = Math.trunc((timer_life - w * 3600) / 60),
+    s = Math.trunc(timer_life - (w * 3600 + m * 60));
+    w = w < 10 ? "0" + w : w;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+  document.querySelector(".time").innerHTML = w + ":" + m + ":" + s;
+}, 1000);
