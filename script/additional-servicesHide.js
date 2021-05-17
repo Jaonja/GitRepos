d88c609c5568771e388fcde1b0e244f65265ad3a
Golishon0 <jaonja69@yandex.ru>
2021-05-17 15:49:57 +0300

    HideBox

---
+++ script/additional-servicesHide.js
@@ -0,0 +1,17 @@
+let $additionalServicesHide = document.querySelector ("#additionalServicesHide")
+let $hideBox = document.querySelector("#hideBox")
+let $upBox = document.querySelector("#upBox")
+
+
+
+
+$additionalServicesHide.addEventListener ("click", function(){
+    $hideBox.style.display = "none"
+    $upBox.style.display = "inline-block"
+    this.style.display = "none"
+})
+$upBox.addEventListener ("click", function(){
+    $hideBox.style.display = "flex"
+    $additionalServicesHide.style.display ="inline-block"
+    this.style.display = "none"
+})
